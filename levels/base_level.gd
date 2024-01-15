2ba0cfdd64a4d1c39ec6b2fd4cc734903a87c591
Sebastian <seblis91@gmail.com>
2024-01-15 22:46:32 +0100

    Launch scene changed to main

--- levels/base_level.gd
+++ levels/base_level.gd
@@ -1,6 +1,6 @@
 extends Node2D
-## For now the script is empty. Most likely some features will be managed
-## here, but we'll see in the future
+## For now the script is almost empty. Most likely some features will be
+## managed here, but we'll see in the future
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +9,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().change_scene_to_file("res://main.tscn")
