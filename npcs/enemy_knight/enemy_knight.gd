6d29e4f4c5c0f86af5c3d0e98350cfa54a06d6b4
Sebastian <seblis91@gmail.com>
2024-01-19 00:57:53 +0100

    Added basic death behaviour to the knight

--- npcs/enemy_knight/enemy_knight.gd
+++ npcs/enemy_knight/enemy_knight.gd
@@ -6,8 +6,7 @@ var player
 
 func _ready():
 	pass
-		
-	
+
 func _physics_process(delta):
 	var move_to_player = (player.global_position - global_position).normalized()
 	velocity.x = move_to_player.x * SPEED
@@ -18,3 +17,6 @@ func _physics_process(delta):
 func set_player_node(player_node):
 	player = player_node
 	
+
+func _on_entity_died():
+	queue_free()
