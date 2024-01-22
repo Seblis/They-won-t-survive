95f072adf0d675995c6093531dd44be92e1394a5
Sebastian <seblis91@gmail.com>
2024-01-22 22:51:55 +0100

    Cleric state machine designed
    For now cleric chooses random corrupted tile and walks until certain range. I still need to implement creating actual attack area

--- global/corruption_engine.gd
+++ global/corruption_engine.gd
@@ -7,25 +7,52 @@ extends Node
 ## it's 
 
 const CORRUPTION_LAYER: int = 1
+const C_SCORE_MULTIPLIER: int = 10
+const B_SCORE_MULTIPLIER: int = -5
 enum TileType {STANDARD, CORRUPT, HOLY}
-#@onready var _map: TileMap = $"../TileMap"
+
+var corrupted_tiles: Dictionary = {}
+var blessed_tiles: Dictionary = {}
+var _map: TileMap
 
 func _ready():
 	SignalManager.on_new_corruption_weapon.connect(connect_corrupt)
 	SignalManager.on_new_bless_weapon.connect(connect_bless)
 
+func reset_tiles(map):
+	_map = map
+	corrupted_tiles.clear()
+	blessed_tiles.clear()
+
 func corrupt_tile(body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int):
 	if body is TileMap:
-		body.set_cell(TileType.CORRUPT, body.get_coords_for_body_rid(body_rid),
-			CORRUPTION_LAYER, Vector2i(randi_range(0,3), randi_range(0,2)), 0)
+		var coords = body.get_coords_for_body_rid(body_rid)
+		corrupted_tiles[body_rid] = null
+		blessed_tiles.erase(body_rid)
+		body.set_cell(TileType.CORRUPT, coords, CORRUPTION_LAYER,
+			Vector2i(randi_range(0,3), randi_range(0,2)), 0)
 
 func connect_corrupt(corrupt_attack: Area2D):
 	corrupt_attack.body_shape_entered.connect(corrupt_tile)
 
 func bless_tile(body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int):
 	if body is TileMap:
-		body.set_cell(TileType.HOLY, body.get_coords_for_body_rid(body_rid),
-		CORRUPTION_LAYER, Vector2i(randi_range(0,3), randi_range(0,2)), 0)
+		var coords = body.get_coords_for_body_rid(body_rid)
+		blessed_tiles[body_rid] = null
+		corrupted_tiles.erase(body_rid)
+		body.set_cell(TileType.HOLY, coords, CORRUPTION_LAYER,
+			Vector2i(randi_range(0,3), randi_range(0,2)), 0)
 
 func connect_bless(bless_attack: Area2D):
 	bless_attack.body_shape_entered.connect(bless_tile)
+
+func rand_corrupted_tile_pos():
+	if not corrupted_tiles:
+		return null
+	var size = corrupted_tiles.size()
+	var random_rid = corrupted_tiles.keys()[randi() % size]
+	print("Random key is ", random_rid)
+	return _map.map_to_local(_map.get_coords_for_body_rid(random_rid))
+	
+
+
--- npcs/enemy_cleric/enemy_cleric.gd
+++ npcs/enemy_cleric/enemy_cleric.gd
@@ -1,25 +1,53 @@
 extends CharacterBody2D
 
 const SPEED: float = 125.0
-const DESIRED_RANGE: float = 72.0
+const DESIRED_RANGE: float = 128.0
+const BLESS_SCENE = preload("res://npcs/enemy_cleric/attack_bless.tscn")
+const WAIT_TIME: float = 6.0
 
-enum State {WALK, BLESS, DEATH}
+enum State {WALK, BLESS, WAIT, DEATH}
+
 var state: int = State.WALK
-var BLESS_SCENE = preload("res://npcs/enemy_cleric/attack_bless.tscn")
+var _target_position
+var _remaining_wait: float = 0
+
 
 func _ready():
-	pass
+	_initiate_walk()
 
-func _physics_process(_delta):
-	pass
+func _physics_process(delta):
+	if state == State.WALK:
+		_update_walk()
+	elif state == State.BLESS:
+		_update_bless()
+	elif state == State.WAIT:
+		_update_wait(delta)
 
 func _initiate_walk():
-	# TODO: connect bless animation finished
-	# TODO: search for corrupted tile
 	state = State.WALK
+	# TODO: connect bless animation finished
+	_target_position = CorruptionEngine.rand_corrupted_tile_pos()
+	if _target_position == null:
+		_initiate_wait()	
 
 func _update_walk():
-	pass
+	var move_to_target = _target_position - global_position
+	
+	print("Len is ", move_to_target.length())
+	if move_to_target.length() < DESIRED_RANGE:
+		_initiate_bless()
+		return
+	
+	move_to_target = move_to_target.normalized()
+	velocity.x = move_to_target.x * SPEED
+	velocity.y = move_to_target.y * SPEED
+	
+	#if velocity.x > 0:
+		#sprite_2d.flip_h = false
+	#elif velocity.x < 0:
+		#sprite_2d.flip_h = true
+		
+	move_and_slide()
 
 func _initiate_bless():
 	state = State.BLESS
@@ -27,6 +55,15 @@ func _initiate_bless():
 
 func _update_bless():
 	pass
+
+func _initiate_wait():
+	state = State.WAIT
+	_remaining_wait = WAIT_TIME
+
+func _update_wait(delta):
+	_remaining_wait -= delta
+	if _remaining_wait <= 0:
+		_initiate_walk()
 
 func _on_entity_died():
 	state = State.DEATH
