4592f0ca554840e3c6e7340e474a7fdee03493da
Sebastian <seblis91@gmail.com>
2024-01-23 20:41:30 +0100

    Tweaked sound volume levels

--- levels/FireballGenerator.gd
+++ levels/FireballGenerator.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 const fireball_scene: PackedScene = preload("res://player/weapons/fireball.tscn")
 @onready var player: Node2D = $"../Player"
-@onready var sounds = $Sounds
+@onready var fireball_sound = $FireballSound
 
 
 
 func _on_fireball_timer_timeout():
 	var new_fireball = fireball_scene.instantiate()
-	sounds.play()
+	fireball_sound.play()
 	new_fireball.set_player_position(player.global_position)
 	
 	add_child(new_fireball)
