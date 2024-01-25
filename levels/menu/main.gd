5ba715b88252dd3f60dd5893d5b9e261fab16a95
Sebastian <seblis91@gmail.com>
2024-01-25 18:56:22 +0100

    Trigger for High Scores screen set to "S" key for now

--- levels/menu/main.gd
+++ levels/menu/main.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const SCORES_POPUP = preload("res://levels/scores_screen/high_score_table.tscn")
 
 func _on_play_btn_pressed():
 	get_tree().change_scene_to_file("res://levels/base_level.tscn")
@@ -11,3 +12,10 @@ func _on_quit_btn_pressed():
 
 func _on_settings_btn_pressed():
 	get_tree().change_scene_to_file("res://levels/settings/settings_scene.tscn")
+	
+
+func _input(event):
+	if event is InputEventKey:
+		if event.is_pressed() and event.keycode == KEY_S:
+			var high_scores = SCORES_POPUP.instantiate()
+			add_child(high_scores)
